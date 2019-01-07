489992671183cc667cb5890a14dacd9a85ebc0b2
pascal65536 <pascal65536@gmail.com>
2019-01-08 02:14:16 +0700

    create

--- printing005.py
+++ printing005.py
@@ -3,7 +3,7 @@ import io
 import json
 
 
-def write_hide(out, name, all_causes):
+def write_hide(out, name, all_cause):
     '''
     Выводит в html-файл раскрывашку с карточками внутри
     :param out: файл
@@ -11,33 +11,26 @@ def write_hide(out, name, all_causes):
     :param all_causes: список отключений
     :return: ничего
     '''
+    out.write('\n\n\n')
     out.write(template.hid_begin)
     out.write(name)
     out.write(template.hid_med)
+    out.write('\n\n\n')
 
     out.write('<div class="flexbox">\n')
-    for all_cause in all_causes:
-        if all_cause['ron'] == name:
-            out.write('<div class="productcard"><div class="productpic">\n')
-            pic = template.pictures[all_cause["res"].strip()]
+    out.write('<div class="productcard"><div class="productpic">\n')
+    out.write(f'<img src="{all_cause["cause_picture"]}" class="image" width="300" height="225">')
+    out.write('</div><div class="producttext">\n')
+    out.write(f'<p><b>Что отключат:</b> {all_cause["cause_resource"]}<p>')
+    out.write(f'<p><b>Организация:</b> {all_cause["cause_company"]}<p>')
+    if 'cause_company_phone' in all_cause.keys():
+        out.write(f'<p>{all_cause["cause_company_phone"]}<p>')
+    out.write(f'<p><b>Тип отключения:</b> {all_cause["type_cause"]}<p>')
+    out.write(f'<p><b>Причина отключений:</b> {all_cause["cause"]}<p>')
+    out.write(f'<p><b>Когда отключат:</b> {all_cause["begin_time"]}<p>')
+    out.write(f'<p><b>Когда включат:</b> {all_cause["end_time"]}<p>')
 
-            out.write(f'<img src="{pic}" class="image" width="300" height="225">')
-            out.write('</div><div class="producttext">\n')
-            if not all_cause["res"] == "":
-                out.write(f'<p><b>Что отключат:</b> {all_cause["res"]}<p>')
-
-            out.write(f'<p><b>Организация:</b> {all_cause["org"]}<p>')
-
-            if not all_cause["tel"] == '':
-                out.write(f'<p><b>Телефон:</b> {all_cause["tel"]}<p>')
-
-            out.write(f'<p><b>Адреса домов:</b> {all_cause["adr"]}<p>')
-            out.write(f'<p><b>Сколько домов:</b> {all_cause["hou"]}<p>')
-            out.write(f'<p><b>Причина отключений:</b> {all_cause["cau"]}<p>')
-            out.write(f'<p><b>Когда отключат:</b> {all_cause["begin_time"]}<p>')
-            out.write(f'<p><b>Когда включат:</b> {all_cause["end_time"]}<p>')
-
-            out.write('</div></div>\n')
+    out.write('</div></div>\n')
     out.write('</div>\n')
     out.write(template.hid_end)
 
@@ -50,17 +43,12 @@ def write_file(causes):
         out.write(template.style)
         out.write(template.start)
 
-        print( causes['Октябрьский'][1] )
+        for region in template.regions:
+            if len(causes[region][1]['value']) > 1:
+                name = causes[region][0]['value']['cause_region'].strip()
+                all_causes = causes[region][1]['value']
+                write_hide(out, name, all_causes)
 
-        #for region in template.regions:
-        #    print ( region )
-
-        '''
-        for name in regions:
-            if not name == '':
-                if number(name, all_causes) != 0:
-                    write_hide(out, name, all_causes)
-        '''
         out.write('\n\n\n')
         out.write(template.footer)
 
